f4567a6dd0aea2cb5c36c0898eb7f80de168d74b
JotheeshKumar <80279461+JotheeshKumar@users.noreply.github.com>
2021-03-27 22:28:21 +0530

    Add files via upload

---
+++ system_INHERITENCE/RunnerClass.java
@@ -0,0 +1,10 @@
+package org.system_INHERITENCE;
+
+public class RunnerClass {
+
+	public static void main(String[] args) {
+		Computer System1 = new Computer();
+		System1.computerModel();
+	}
+
+}
